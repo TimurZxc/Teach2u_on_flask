e5fdac37243a56dfbaa8eea89f114bc5776ac499
Timur <72609967+TimurZxc@users.noreply.github.com>
2022-11-25 05:36:06 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from app import app
 import os
 
 if __name__ == (__name__):
-   port = int(os.environ.get('PORT', 5000)) 
+   port = int(os.getenv('PORT'))
    app.run(host='0.0.0.0', port=port)
    
