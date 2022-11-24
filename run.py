6ce77448aa9e4a78425bf8288b615d0fb52a9c57
Timur <timur.grig.kz.2002@gmail.com>
2022-11-25 03:46:59 +0600

    run

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import app
-from gevent.pywsgi import WSGIServer
 
 if __name__ == (__name__):
-   # app.run(host="0.0.0.0", port=5000)
-   http_server = WSGIServer(('', 5000), app)
-   http_server.serve_forever()
+   # app.run(host="0.0.0.0")
+   from waitress import serve
+   serve(app, listen='*:8080')
