72ba7405fe93a2e3c8b2a13d325f4718b2ebfd2d
Timur <timur.grig.kz.2002@gmail.com>
2022-11-25 03:23:10 +0600

    app

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 from app import app
+from gevent.pywsgi import WSGIServer
+
 if __name__ == (__name__):
-   app.run(host="0.0.0.0", port=5000)
+   # app.run(host="0.0.0.0", port=5000)
+   http_server = WSGIServer(('', 5000), app)
+   http_server.serve_forever()
